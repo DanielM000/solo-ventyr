6c103d3d34ad14bdbb9b10d2f8cc6c9ab9600d76
Daniel Mohammadi <daniel.mohammadi@learnet.se>
2024-12-11 16:02:20 +0100

    Version 5

--- soloäventyr.py
+++ soloäventyr.py
@@ -12,7 +12,6 @@ def slow_print(text):
         time.sleep(0.03)
     print()
 menu = True
-mellansekvens_1 = False
 spelet_körs = False
 
 
@@ -23,7 +22,7 @@ while menu:
     menu_val = input("Vad vill du göra: ")
     if menu_val == "1":
         slow_print("Startar spelet...")
-        mellansekvens_1 = True
+        spelet_körs = True
         menu = False
     elif menu_val == "2":
         slow_print("Lämnar spelet...")
@@ -32,10 +31,8 @@ while menu:
         slow_print("Ogiltigt, försök igen.")
         continue
 
-while mellansekvens_1:
-    slow_print("Du hittar dig själv inne i en cell med en säng, en papperskorg, ett fönster\noch en sink, dörren är låst men den har ett nyckelhål, hitta ett sätt att fly.")
-    spelet_körs = True
-    mellansekvens_1 = False
+def mellansekvens_1():
+    slow_print("Du befinner dig själv inne i en cell med en säng, en papperskorg, ett fönster\noch en sink, dörren är låst men den har ett nyckelhål, hitta ett sätt att fly.")
 
 def mellansekvens_2():
     slow_print("Du smyger framåt tills du närmar en korridor, plötsligt ser du en vakt och \ndu kan inte gå förbi utan att bli upptäckt.")
@@ -44,19 +41,21 @@ def mellansekvens_2():
 def cell(rum):
     spel = True
     nyckel = False
+    mellansekvens_1()
     
     while spel:
         cell_val = input("1. Kolla under sängen\n2. Kolla in i papperskorgen\n3. Kolla på fönstret\n4. Kolla in i sinken\n5. Öppna dörren\nVad vill du göra: ")
         if cell_val == "1" or cell_val == "2" or cell_val == "3":
             slow_print("Du hittar inget.")
-        elif cell_val == "4":
+        elif cell_val == "4" and nyckel == False:
             slow_print("Du hittar en nyckel.")
             nyckel = True
+        elif cell_val == "4" and nyckel == True:
+            slow_print("Du hittar inget.")
         elif cell_val == "5" and nyckel == False:
             slow_print("Dörren är låst.")
         elif cell_val == "5" and nyckel == True:
             slow_print("Du öppnar dörren med nyckeln och smitter förbi...")
-            mellansekvens_2()
             rum = "korridor"
             return rum
         else:
@@ -67,15 +66,34 @@ def cell(rum):
 def korridor(rum):
     spel = True
     vakt_borta = False
-    disraktion = False
+    distraktion = False
+    mellansekvens_2()
 
     while spel:
-        korridor_val = input("1. Smyg förbi\n2. Leta efter något i marken\n3. Vänta\n4. Distrahera vakten\nVad vill du göra: ")
-        if korridor == "1" and vakt_borta == False:
-            slow_print("Vakten ser dig och fångar dig, han drar dig tillbaka till din cell")
+        korridor_val = input("1. Smyg förbi\n2. Leta efter något i marken\n3. Distrahera vakten\nVad vill du göra: ")
+        if korridor_val == "1" and vakt_borta == False:
+            slow_print("Vakten ser dig och fångar dig, han drar dig tillbaka till din cell...")
             rum = "cell"
             return rum
-        elif 
+        elif korridor_val == "1" and vakt_borta == True:
+            slow_print("Du smitter genom korridoren...")
+            rum = "slut"
+            return rum
+        elif korridor_val == "2" and distraktion == False:
+            slow_print("Du hittar en sten.")
+            distraktion = True
+        elif korridor_val == "2" and distraktion == True:
+            slow_print("Du hittar inget.")
+        elif korridor_val == "3" and distraktion == False and vakt_borta == False:
+            slow_print("Du har ingeting att distrahera med.")
+        elif korridor_val == "3" and distraktion == True and vakt_borta == False:
+            slow_print("Du kastar stenen bakom dig och gömmer dig vid en hörn, vakten går mot ljudet utan att se dig.")
+            vakt_borta = True
+        elif korridor_val == "3" and distraktion == True and vakt_borta == True:
+            slow_print("Vakten är redan borta.")
+        else:
+            slow_print("Ogiltigt, försök igen.")
+            continue
 
 # Spel loop
 while (spelet_körs == True):
