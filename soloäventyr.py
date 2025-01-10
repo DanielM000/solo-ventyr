4ef3c5f966999c1024ee4f679bb56d096e5a16cf
Daniel Mohammadi <daniel.mohammadi@learnet.se>
2025-01-10 15:04:58 +0100

    Version 7

--- soloäventyr.py
+++ soloäventyr.py
@@ -18,7 +18,7 @@ spelet_körs = False
 
 
 while menu:
-    slow_print("1. Starta spelet\n2. Avsluta")
+    slow_print("1. Starta spelet\n2. Avsluta\n3. Visa instruktioner")
     menu_val = input("Vad vill du göra: ")
     if menu_val == "1":
         slow_print("Startar spelet...")
@@ -27,6 +27,21 @@ while menu:
     elif menu_val == "2":
         slow_print("Lämnar spelet...")
         menu = False
+    elif menu_val == "3":
+        print("\n")
+        filnamn = "readme.txt"
+
+        try:
+            with open(filnamn, "r", encoding="utf-8") as fil:
+                innehål = fil.read()
+                print(innehål)
+                input("\nTryck på valfri tangent")
+        except FileNotFoundError:
+            print(f"Filen {filnamn} kunde inte hittas")
+            input("\nTryck på valfri tangent")
+        except Exception as e:
+            print(f"Ett fel inträffade: {e}")
+            input("\nTryck på valfri tangent")
     else:
         slow_print("Ogiltigt, försök igen.")
         continue
@@ -39,6 +54,9 @@ def mellansekvens_2():
 
 def mellansekvens_3():
     slow_print("Du fortsätter smygga fram i korridoren tills du ser två vägar, en till vänster\noch en till höger.")
+
+def mellansekvens_4():
+    slow_print("Du närmar dig ett rum men plötsligt ser du en hund som vaktar en dörr,\ndu kan inte gå förbi utan att bli upptäckt.")
 
 # RUM 1
 def cell(rum):
@@ -111,14 +129,53 @@ def vägar(rum):
             return rum
         elif vägar_val == "2":
             slow_print("Du går höger...")
-            rum = "slut"
+            rum = "hund"
             return rum
         else:
             slow_print("Ogiltigt, försök igen.")
             continue
 
-#RUM 4
+# RUM 4 INTE KLAR
+def hund(rum):
+    spel = True
+    hund_borta = False
+    distraktion = False
+    mellansekvens_4()
 
+    while spel:
+        hund_val = input("1. Gå fram\n2. Leta efter något i marken\n3. Distrahera hunden\nVad vill du göra: ")
+        if hund_val == "1" and hund_borta == False:
+            slow_print("Hunden ser dig och biter dig i benen, vakten kommer och han drar dig tillbaka till din cell...")
+            rum = "cell"
+            return rum
+        elif hund_val == "1" and hund_borta == True:
+            slow_print("Du går till dörren utan problem, du öppnar dörren...")
+            rum = "slut"
+            return rum
+        elif hund_val == "2" and distraktion == False:
+            slow_print("Du hittar en liten boll.")
+            distraktion = True
+        elif hund_val == "2" and distraktion == True:
+            slow_print("Du hittar inget.")
+        elif hund_val == "3" and distraktion == False and hund_borta == False:
+            slow_print("Du har ingeting att distrahera med.")
+        elif hund_val == "3" and distraktion == True and hund_borta == False:
+            boll_kast: input("1. Kasta bollen kort fram\n2. Kasta bollen långt fram\nVad vill du göra: ")
+            if boll_kast == "1":
+                slow_print("Hunden går fram, tar bollen och går iväg med den.")
+                hund_borta = True
+            elif boll_kast == "2":
+                slow_print ("Hunden går fram, tar bollen men går fram mot dig och ser dig, hunden biter dig i benen och vakten kommer, han drar dig tillbaka till din cell...")
+                rum = "cell"
+                return rum
+            else:
+                slow_print("Ogiltigt, försök igen.")
+                continue
+        elif hund_val == "3" and distraktion == True and hund_borta == True:
+            slow_print("Vakten är redan borta.")
+        else:
+            slow_print("Ogiltigt, försök igen.")
+            continue
 
 # Spel loop
 while (spelet_körs == True):
@@ -128,5 +185,7 @@ while (spelet_körs == True):
        rum = korridor(rum)
     elif (rum == "vägar"):
         rum = vägar(rum)
+    elif (rum == "hund"):
+        rum = hund(rum)
     elif (rum == "slut"):
         break
